a6dcd7e58c38c4b913fbe308eb5c1567625255f4
Emestay <103575914+Emestay@users.noreply.github.com>
2023-04-05 09:21:26 +0200

    Routeur et vue depuis les controller

--- test.sql
+++ test.sql
@@ -1,93 +1,126 @@
+-- création de la base de données
+create database chocoblast;
+use chocoblast;
 
-INSERT INTO equipe (nom_equipe)
-VALUES ('Albi'),
-       ('Castres'),
-       ('Toulouse'),
-       ('Paris'),
-       ('Marseille');
+-- création des tables
+create table utilisateur(
+id_utilisateur int primary key auto_increment not null,
+nom_utilisateur varchar(50) not null,
+prenom_utilisateur varchar(50) not null,
+mail_utilisateur varchar(50) not null,
+password_utilisateur varchar(100) not null,
+image_utilisateur varchar(100) null,
+statut_utilisateur tinyint(1) default 0,
+id_roles int null
+)Engine=InnoDB;
 
+create table roles(
+id_roles int primary key auto_increment not null,
+nom_roles varchar(50) not null
+)Engine=InnoDB;
 
+create table chocoblast(
+id_chocoblast int primary key auto_increment  not null,
+slogan_chocoblast text not null,
+date_chocoblast date not null,
+statut_chocoblast tinyint(1) default 0,
+cible_chocoblast int not null,
+auteur_chocoblast int not null
+)Engine=InnoDB;
 
-INSERT INTO joueur (nom_joueur, prenom_joueur, id_equipe)
-VALUES ('Becker', 'Paul', 1),
-       ('Lagarde', 'Pauline', 1),
-       ('Levy', 'Bernard', 1),
-       ('Renard', 'Manon', 2),
-       ('Nicolas-Chevallier', 'Gilbert', 2),
-       ('Weber-Francois', 'Cécile', 2),
-       ('Cousin', 'Patricia', 3),
-       ('Dijoux', 'Nicolas', 3),
-       ('Thomas', 'Marc', 3),
-       ('Levy', 'Martin', 3);
+create table commentaire(
+id_commentaire int primary key auto_increment not null,
+note_commentaire int not null,
+text_commentaire text not null,
+date_commentaire datetime not null,
+statut_commentaire tinyint(1) default 0,
+id_chocoblast int not null,
+auteur_commentaire int not null
+)Engine=InnoDB;
 
-INSERT INTO partie (date_match)
-VALUES ('2023-01-12'),
-       ('2023-01-17'),
-       ('2023-02-14'),
-       ('2023-02-16'),
-       ('2023-03-03');
+create table boulangerie(
+id_boulangerie int primary key auto_increment not null,
+nom_boulangerie varchar(100) not null,
+horaires_boulangerie varchar(100) not null,
+lien_google_maps text not null
+)Engine=InnoDB;
 
+create table utilisateur_boulangerie(
+id_utilisateur_boulangerie int primary key auto_increment not null,
+id_utilisateur int not null,
+id_boulangerie int not null
+)Engine=InnoDB;
 
-INSERT INTO stat(id_match, id_joueur, but, passe, carton)
-VALUES (1, 1, 2, 10, 1),
-       (1, 2, 3, 8, 0),
-       (2, 3, 1, 6, 0),
-       (2, 4, 2, 9, 1),
-       (3, 4, 4, 9, 1),
-       (3, 5, 0, 4, 0),
-       (3, 6, 0, 12, 1),
-       (4, 7, 2, 1, 0),
-       (4, 8, 3, 14, 2),
-       (5, 9, 9, 30, 2),
-       (5, 10, 1, 0, 0);
-
-INSERT INTO score(id_equipe1, id_equipe2, id_match, but1, but2)
-VALUES (1, 2, 1, 2, 3),
-       (2, 3, 2, 6, 3),
-       (4, 5, 5, 8, 1);
-
--- update du Martin
-UPDATE joueur
-SET id_equipe = 3
-WHERE nom_joueur < 'Martin';
-
--- Ciotti.sql
-
-UPDATE equipe
-SET nom_equipe = 'Nice'
-WHERE id_equipe = 4;
-
--- cheat code +2 buts
-
-UPDATE score
-SET but1 = but1 + 2
-WHERE id_equipe1 = 1;
-
--- plus de passes pour plus de buts
-
-UPDATE stat
-SET but = but + 3
-WHERE passe > 4;
-
--- moins de passes moins de buts du coup
-
-UPDATE stat
-SET but = but - 1
-WHERE passe < 2;
-
-UPDATE score
-SET but1 = but1 + 1
-WHERE id_equipe1 IN (SELECT id_equipe FROM equipe WHERE nom_equipe = 'Toulouse');
-
-UPDATE score
-SET but2 = but2 + 1
-WHERE id_equipe2 IN (SELECT id_equipe FROM equipe WHERE nom_equipe = 'Toulouse');
-
--- Suite du 04/04/23
-
--- F: " jaime pas les gens bons"
--- Wass: "prends du salami"
+CREATE TABLE vote_rat (
+  id_vote_rat int primary key auto_increment not null,
+  id_utilisateur_votant int not null,
+  id_utilisateur_candidat int not null,
+  date_vote datetime not null
+) Engine=InnoDB;
 
 
+-- ajout des contraintes
+alter table utilisateur
+add constraint fk_posseder_roles
+foreign key(id_roles)
+references roles(id_roles);
+
+alter table chocoblast
+add constraint fk_chocoblaster_chocoblast
+foreign key(auteur_chocoblast)
+references utilisateur(id_utilisateur);
+
+alter table chocoblast
+add constraint fk_cibler_chocoblast
+foreign key(cible_chocoblast)
+references utilisateur(id_utilisateur);
+
+alter table commentaire
+add constraint fk_poster_commentaire
+foreign key(auteur_commentaire)
+references utilisateur(id_utilisateur);
+
+alter table commentaire
+add constraint fk_rattacher_commentaire
+foreign key(id_chocoblast)
+references chocoblast(id_chocoblast);
+
+alter table utilisateur_boulangerie
+add constraint fk_utilisateur_boulangerie
+foreign key(id_utilisateur)
+references utilisateur(id_utilisateur);
+
+alter table utilisateur_boulangerie
+add constraint fk_boulangerie_utilisateur
+foreign key(id_boulangerie)
+references boulangerie(id_boulangerie);
+
+-- Suite modif 
+
+ALTER TABLE utilisateur
+ADD est_rat boolean DEFAULT false;
+
+ALTER TABLE vote_rat
+ADD CONSTRAINT fk_utilisateur_votant
+FOREIGN KEY (id_utilisateur_votant)
+REFERENCES utilisateur (id_utilisateur);
+
+ALTER TABLE vote_rat
+ADD CONSTRAINT fk_utilisateur_candidat
+FOREIGN KEY (id_utilisateur_candidat)
+REFERENCES utilisateur (id_utilisateur);
 
 
+/* ?TODO ==> Requete SQL du vote et de l'update
+
+SELECT id_utilisateur_candidat, COUNT(*) as nb_votes
+FROM vote_rat
+GROUP BY id_utilisateur_candidat
+ORDER BY nb_votes DESC
+LIMIT 1;
+
+UPDATE utilisateur
+SET est_rat = true
+WHERE id_utilisateur = :id_utilisateur;
+
+*/
