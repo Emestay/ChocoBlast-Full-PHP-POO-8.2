1183d268f24640d46f9fb681613f7a1625af2fe0
Emestay <103575914+Emestay@users.noreply.github.com>
2023-04-05 10:11:59 +0200

    Ajout page 404

--- app/vue/view_404.php
+++ app/vue/view_404.php
@@ -154,18 +154,26 @@
     }
     </style>
 <script>
-    // js source https://codepen.io/moklick/pen/zKleC
 
-    var Application = ( function () {
-        var canvas;
-        var ctx;
-        var imgData;
-        var pix;
-        var WIDTH;
-        var HEIGHT;
-        var flickerInterval;
+    const Application = (function () {
+        const flickering = function () {
+            for (let i = 0; i < pix.length; i += 4) {
+                const color = (Math.random() * 255) + 50;
+                pix[i] = color;
+                pix[i + 1] = color;
+                pix[i + 2] = color;
+            }
+            ctx.putImageData(imgData, 0, 0);
+        };
+        let canvas;
+        let ctx;
+        let imgData;
+        let pix;
+        let WIDTH;
+        let HEIGHT;
+        let flickerInterval;
 
-        var init = function () {
+        const init = function () {
             canvas = document.getElementById('canvas');
             ctx = canvas.getContext('2d');
             canvas.width = WIDTH = 700;
@@ -178,15 +186,6 @@
             flickerInterval = setInterval(flickering, 30);
         };
 
-        var flickering = function () {
-            for (var i = 0; i < pix.length; i += 4) {
-                var color = (Math.random() * 255) + 50;
-                pix[i] = color;
-                pix[i + 1] = color;
-                pix[i + 2] = color;
-            }
-            ctx.putImageData(imgData, 0, 0);
-        };
 
         return {
             init: init
